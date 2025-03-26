c5b380425516baad9710700afc594d9068f4854a
Tigist Amakelew <tigistamakelew156@gmail.com>
2025-03-26 18:39:59 +0300

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'To-Do-list',
+  base: '/To-Do-list/', // Ensure the base URL includes leading and trailing slashes
   plugins: [react()],
 })
